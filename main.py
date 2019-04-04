d890c7a37daf29e4a7a65879a1d74582eb1abae9
Aldiantoro Nugroho <kriwil@gmail.com>
2019-04-05 06:53:24 +0700

    basic latest builds display

---
+++ main.py
@@ -0,0 +1,38 @@
+import urllib
+import requests
+import os
+
+
+API_BASE_URL = "https://circleci.com/api/v1.1/"
+GREEN = "✔️"
+RED = "❌"
+GREY = "⚪"
+
+
+def get_token():
+    with open(os.path.join(os.getcwd(), ".statusci")) as f:
+        return f.read().strip()
+
+
+def get_status(status):
+    if status in ["success", "fixed"]:
+        return GREEN
+    elif status in ["failed", "timedout"]:
+        return RED
+    return GREY
+
+
+def main():
+    token = get_token()
+    url = f"{API_BASE_URL}recent-builds"
+    params = {"circle-token": token}
+    response = requests.get(url, params=params)
+    content = response.json()
+    for build in content:
+        repo = f"{build['username']}/{build['reponame']}/{build['branch']}"
+        status = get_status(build["status"])
+        print(f"{status} {repo}")
+
+
+if __name__ == "__main__":
+    main()
